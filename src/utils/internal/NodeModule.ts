c6dfc10e185a5a0fd5c4149558d513a9cc3e2615
Jeongho Nam <samchon.github@gmail.com>
2022-08-15 01:38:02 +0900

    Complement #53, avoid dynamic import transpiling

--- src/utils/internal/NodeModule.ts
+++ src/utils/internal/NodeModule.ts
@@ -1,16 +1,34 @@
+import include from "import2";
+import type * as __cp from "child_process";
 import type * as __fs from "fs";
+import type * as __http from "http";
+import type * as __https from "https";
+import type * as __os from "os";
+import type * as __thread from "worker_threads";
 import type * as __ws from "ws";
 
 import { Singleton } from "tstl/thread/Singleton";
 
 export namespace NodeModule {
-    export const cp = new Singleton(() => import("child_process"));
-    export const fs: Singleton<Promise<typeof __fs>> = new Singleton(
-        () => import("fs"),
+    export const cp: Singleton<Promise<typeof __cp>> = new Singleton(() =>
+        include("child_process"),
     );
-    export const http = new Singleton(() => import("http"));
-    export const https = new Singleton(() => import("https"));
-    export const os = new Singleton(() => import("os"));
-    export const thread = new Singleton(() => import("worker_threads"));
-    export const ws = new Singleton<Promise<typeof __ws>>(() => import("ws"));
+    export const fs: Singleton<Promise<typeof __fs>> = new Singleton(() =>
+        include("fs"),
+    );
+    export const http: Singleton<Promise<typeof __http>> = new Singleton(() =>
+        include("http"),
+    );
+    export const https: Singleton<Promise<typeof __https>> = new Singleton(() =>
+        include("https"),
+    );
+    export const os: Singleton<Promise<typeof __os>> = new Singleton(() =>
+        include("os"),
+    );
+    export const thread: Singleton<Promise<typeof __thread>> = new Singleton(
+        () => include("worker_threads"),
+    );
+    export const ws: Singleton<Promise<typeof __ws>> = new Singleton(() =>
+        include("ws"),
+    );
 }
