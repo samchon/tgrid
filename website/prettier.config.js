8c7a10bb9b72708a2fec2b27193b7fcbc914dadb
Jeongho Nam <samchon.github@gmail.com>
2024-05-06 22:16:32 +0900

    Writing #68: guide documents again

---
+++ website/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  // importOrderParserPlugins: ["decorators-legacy", "typescript", "jsx"],
+};
---
+++ website/theme.config.tsx
@@ -0,0 +1,116 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
+
+const config: DocsThemeConfig = {
+  logo: () => (
+    <>
+      <img src="/favicon/android-chrome-192x192.png" width={32} height={32} />
+      <span
+        style={{
+          fontWeight: "bold",
+          fontSize: "1.2rem",
+          paddingLeft: 15,
+          paddingRight: 10,
+        }}
+      >
+        TGrid
+      </span>
+    </>
+  ),
+  // nextThemes: {
+  //   defaultTheme: "dark",
+  // },
+  project: {
+    link: "https://github.com/samchon/tgrid",
+  },
+  docsRepositoryBase: "https://github.com/samchon/tgrid/blob/master/website",
+  footer: {
+    text: () => (
+      <span>
+        Released under the MIT License.
+        <br />
+        <br />
+        Copyright 2018 - {new Date().getFullYear()}{" "}
+        <a
+          href="https://github.com/samchon"
+          target="_blank"
+          style={{ color: "initial" }}
+        >
+          <u>Samchon</u>
+        </a>{" "}
+        & Contributors
+      </span>
+    ),
+  },
+  useNextSeoProps() {
+    return {
+      defaultTitle: "TGrid Guide Documents",
+      titleTemplate: "TGrid Guide Documents - %s",
+      additionalLinkTags: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "manifest",
+          href: "/favicon/site.webmanifest",
+        },
+        ...[16, 32].map((size) => ({
+          rel: "icon",
+          type: "image/png",
+          sizes: `${size}x${size}`,
+          href: `/favicon/favicon-${size}x${size}.png`,
+        })),
+      ],
+      additionalMetaTags: [
+        {
+          property: "og:image",
+          content: "/og.jpg",
+        },
+        {
+          property: "og:type",
+          content: "object",
+        },
+        {
+          property: "og:title",
+          content: "Typia Guide Documents",
+        },
+        {
+          property: "og:description",
+          content: "Superfast Runtime Validator with only one line",
+        },
+        {
+          property: "og:site_name",
+          content: "Typia Guide Documents",
+        },
+        {
+          property: "og:url",
+          content: "https://tgrid.com",
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:image",
+          content: "https://tgrid.com/og.jpg",
+        },
+        {
+          name: "twitter:title",
+          content: "TGrid Guide Documents",
+        },
+        {
+          name: "twitter:description",
+          content: "Superfast Runtime Validator with only one line",
+        },
+        {
+          name: "twitter:site",
+          content: "@SamchonGithub",
+        },
+      ],
+    };
+  },
+};
+
+export default config;
