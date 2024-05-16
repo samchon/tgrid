7ceb1d911124836124d4ead96443884f3d06f57e
Jeongho Nam <samchon.github@gmail.com>
2024-05-17 01:56:12 +0900

    Fix website deploye error

--- website/next-sitemap.config.js
+++ website/next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://typia.io',
-    generateRobotsTxt: true,
+  siteUrl: "https://tgrid.com",
+  generateRobotsTxt: true,
 };
