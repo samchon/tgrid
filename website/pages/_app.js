8a18f3e3e0e2f195b295abc11284b481498a4b3d
Jeongho Nam <samchon.github@gmail.com>
2024-05-17 15:03:33 +0900

    Add clarity analyzer

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -9,10 +9,10 @@ export default function Nextra({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
 (function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "gqhymojzmp");
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "mdafwfogmt");
                     `,
         }}
       />
